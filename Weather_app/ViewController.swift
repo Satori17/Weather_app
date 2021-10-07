70d925f366922e0a9b941d31694358b3b8457401
saba khitaridze <sabaxitara@icloud.com>
2021-10-08 02:32:07 +0400

    Initial Commit

---
+++ Weather_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_app
+//
+//  Created by Saba Khitaridze on 08.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
